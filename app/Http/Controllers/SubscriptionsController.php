35dea56184bb431e772d78dae0112ed0b2a4d77e
mohamednasser-dev <72683413+mohamednasser-dev@users.noreply.github.com>
2021-06-08 13:49:07 +0200

    make is rated

--- app/Http/Controllers/SubscriptionsController.php
+++ app/Http/Controllers/SubscriptionsController.php
@@ -24,30 +24,32 @@ class SubscriptionsController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth:api' , ['except' => ['subscriptions']]);
+        $this->middleware('auth:api', ['except' => ['subscriptions']]);
     }
-    public function subscriptions( Request $request , $type) {
-        $lang = $request->lang ;
-        $user_id = auth()->user()->id;
-  
-        $subscriptions = [];
-        if($type == 'hall'){
-        $data = Reservation::select('id','booking_id','expire_date','price','user_id','status')
-            ->where('type',$type)
-            ->where('user_id',$user_id)
-            ->get()
-            ->map(function($reserv) use ($lang){
-                $reserv->hall_name = $reserv->Booking->Hall->name;
-                $reserv->hall_logo = $reserv->Booking->Hall->logo;
-                if($lang == 'ar'){
-                    $reserv->reserve_name = $reserv->Booking->name_ar;
-                }else{
-                    $reserv->reserve_name = $reserv->Booking->name_en;
-                }
-                return $reserv;
-            });
 
-            foreach ($data as $key => $row){
+    public function subscriptions(Request $request, $type)
+    {
+        $lang = $request->lang;
+        $user_id = auth()->user()->id;
+
+        $subscriptions = [];
+        if ($type == 'hall') {
+            $data = Reservation::select('id', 'booking_id', 'expire_date', 'price', 'user_id', 'status')
+                ->where('type', $type)
+                ->where('user_id', $user_id)
+                ->get()
+                ->map(function ($reserv) use ($lang) {
+                    $reserv->hall_name = $reserv->Booking->Hall->name;
+                    $reserv->hall_logo = $reserv->Booking->Hall->logo;
+                    if ($lang == 'ar') {
+                        $reserv->reserve_name = $reserv->Booking->name_ar;
+                    } else {
+                        $reserv->reserve_name = $reserv->Booking->name_en;
+                    }
+                    return $reserv;
+                });
+
+            foreach ($data as $key => $row) {
                 $subscriptions[$key]['id'] = $row->id;
                 $subscriptions[$key]['booking_id'] = $row->booking_id;
                 $subscriptions[$key]['hall_id'] = $row->Booking->hole_id;
@@ -57,23 +59,30 @@ class SubscriptionsController extends Controller
                 $subscriptions[$key]['price'] = $row->price;
                 $subscriptions[$key]['expire_date'] = $row->expire_date;
                 $subscriptions[$key]['status'] = $row->status;
+                $rates = Rate::where('user_id',$user_id)->where('order_id',$row->Booking->hole_id)->where('type','hall')->first();
+                if($rates == null){
+                    $subscriptions[$key]['isRated'] = false;
+                }else{
+                    $subscriptions[$key]['isRated'] = true;
+                }
+
             }
-        }else if($type == 'coach'){
-            $data = Reservation::select('id','booking_id','expire_date','price','user_id','status')
-                ->where('type',$type)
-                ->where('user_id',$user_id)
+        } else if ($type == 'coach') {
+            $data = Reservation::select('id', 'booking_id', 'expire_date', 'price', 'user_id', 'status')
+                ->where('type', $type)
+                ->where('user_id', $user_id)
                 ->get()
-                ->map(function($reserv) use ($lang){
+                ->map(function ($reserv) use ($lang) {
                     $reserv->coach_name = $reserv->Booking_coach->Coach->name;
                     $reserv->coach_logo = $reserv->Booking_coach->Coach->image;
-                    if($lang == 'ar'){
+                    if ($lang == 'ar') {
                         $reserv->reserve_name = $reserv->Booking_coach->name_ar;
-                    }else{
+                    } else {
                         $reserv->reserve_name = $reserv->Booking_coach->name_en;
                     }
                     return $reserv;
                 });
-            foreach ($data as $key => $row){
+            foreach ($data as $key => $row) {
                 $subscriptions[$key]['id'] = $row->id;
                 $subscriptions[$key]['booking_id'] = $row->booking_id;
                 $subscriptions[$key]['coach_id'] = $row->Booking_coach->coach_id;
@@ -83,47 +92,55 @@ class SubscriptionsController extends Controller
                 $subscriptions[$key]['price'] = $row->price;
                 $subscriptions[$key]['expire_date'] = $row->expire_date;
                 $subscriptions[$key]['status'] = $row->status;
+                $rates = Rate::where('user_id',$user_id)->where('order_id',$row->Booking->hole_id)->where('type','coach')->first();
+                if($rates == null){
+                    $subscriptions[$key]['isRated'] = false;
+                }else{
+                    $subscriptions[$key]['isRated'] = true;
+                }
+
             }
         }
-        $response = APIHelpers::createApiResponse(false , 200 ,  '', '' , $subscriptions, $request->lang );
-        return response()->json($response , 200);
+        $response = APIHelpers::createApiResponse(false, 200, '', '', $subscriptions, $request->lang);
+        return response()->json($response, 200);
     }
 
-    public function payments( Request $request) {
-        $lang = $request->lang ;
+    public function payments(Request $request)
+    {
+        $lang = $request->lang;
         $user_id = auth()->user()->id;
-        $my_balance = User::where('id',$user_id)->select('id' , 'my_wallet as my_balance')->first();
-        $data = Reservation::select('id','booking_id','expire_date','price','user_id','created_at','type')
-            ->where('user_id',$user_id)
-            ->orderBy('created_at','desc')
+        $my_balance = User::where('id', $user_id)->select('id', 'my_wallet as my_balance')->first();
+        $data = Reservation::select('id', 'booking_id', 'expire_date', 'price', 'user_id', 'created_at', 'type')
+            ->where('user_id', $user_id)
+            ->orderBy('created_at', 'desc')
             ->get()
-            ->map(function($reserv) use ($lang){
-                if($reserv->type == 'hall'){
+            ->map(function ($reserv) use ($lang) {
+                if ($reserv->type == 'hall') {
                     $reserv->name = $reserv->Booking->Hall->name;
-                    if($lang == 'ar'){
+                    if ($lang == 'ar') {
                         $reserv->reserve_name = $reserv->Booking->name_ar;
-                    }else{
+                    } else {
                         $reserv->reserve_name = $reserv->Booking->name_en;
                     }
-                }else{
+                } else {
                     $reserv->name = $reserv->Booking_coach->Coach->name;
-                    if($lang == 'ar'){
+                    if ($lang == 'ar') {
                         $reserv->reserve_name = $reserv->Booking_coach->name_ar;
-                    }else{
+                    } else {
                         $reserv->reserve_name = $reserv->Booking_coach->name_en;
                     }
                 }
                 return $reserv;
             });
 
-        foreach ($data as $key => $row){
+        foreach ($data as $key => $row) {
             $subscriptions[$key]['id'] = $row->id;
             $subscriptions[$key]['name'] = $row->name;
             $subscriptions[$key]['price'] = $row->price;
             $subscriptions[$key]['created_at'] = $row->created_at;
         }
 
-        $response = APIHelpers::createApiResponse(false , 200 ,  '', '' , array('my_balance'=> $my_balance , 'history' => $subscriptions) , $request->lang );
-        return response()->json($response , 200);
+        $response = APIHelpers::createApiResponse(false, 200, '', '', array('my_balance' => $my_balance, 'history' => $subscriptions), $request->lang);
+        return response()->json($response, 200);
     }
 }
