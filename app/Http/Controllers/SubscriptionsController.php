ab91bfb48b8c407f6fb7e37d821dece0763a6ee4
mohamednasser-dev <72683413+mohamednasser-dev@users.noreply.github.com>
2021-06-08 09:11:24 +0200

    make empty array in subscribers

--- app/Http/Controllers/SubscriptionsController.php
+++ app/Http/Controllers/SubscriptionsController.php
@@ -29,7 +29,8 @@ class SubscriptionsController extends Controller
     public function subscriptions( Request $request , $type) {
         $lang = $request->lang ;
         $user_id = auth()->user()->id;
-        $subscriptions[] = empty('');
+  
+        $subscriptions = [];
         if($type == 'hall'){
         $data = Reservation::select('id','booking_id','expire_date','price','user_id','status')
             ->where('type',$type)
